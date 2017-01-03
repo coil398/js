8d9941b784ccc90689bf44099ef5ed0dc0ac11df
coil398 <konayuta.sou@gmail.com>
2017-01-04 04:03:17 +0900

    環境できた

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var webserver = require('gulp-webserver');
 
 gulp.task('browserify', function() {
     browserify('./src/app.jsx', { debug: true })
-    .transform(babelify)
+    .transform(babelify, {presets: ['react']})
     .bundle()
     .on('error', function(err) { console.log('Error: ' + err.message); })
     .pipe(source('bundle.js'))
