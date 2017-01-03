c0d5d0cd9cb6cc52b787c35a4d1115fbce22fb57
coil398 <konayuta.sou@gmail.com>
2017-01-04 01:01:01 +0900

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,30 @@
 
 var browserify = require('browserify');
 var gulp = require('gulp');
+var babelify = require('babelify');
 var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var globby = require('globby');
-var through = require('through2');
+var webserver = require('gulp-webserver');
 
-gulp.task('default', function(){
-        var bundledStream = through();
-
-        bundledStream
-        .pipe(source('index.js'))
-        .pipe(buffer())
-        .pipe(gulp.dest('./dist'));
-
-        globby(['./src/*.js']).then(function(entries){
-            var b = browserify({
-                entries: entries,
-            });
-
-            b.bundle().pipe(bundledStream);
-    }).catch(function(err){
-        bundledStream.emit('error', err);
-    });
-
-    return bundledStream;
+gulp.task('browserify', function() {
+    browserify('./src/app.jsx', { debug: true })
+    .transform(babelify)
+    .bundle()
+    .on('error', function(err) { console.log('Error: ' + err.message); })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./dist/'))
 });
 
 gulp.task('watch', function(){
-    gulp.watch('./src/*.js',['default']);
+    gulp.watch('./src/*.jsx', ['browserify'])
 });
+
+gulp.task('webserver', function(){
+    gulp.src('./')
+    .pipe(webserver({
+        host: 'localhost',
+        livereload: true
+    })
+);
+});
+
+gulp.task('default', ['browserify', 'watch', 'webserver']);
